8536d9824c6fc919d1a74b526569b80a6d018530
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-07-14 18:34:36 +0700

    rahmatupdateat1833

---
+++ NC3Facewash WatchKit Extension/Rahmattest1803.swift
@@ -0,0 +1,20 @@
+//
+//  Rahmattest1803.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Rahmat Maftuh Ihsan on 14/07/22.
+//
+
+import SwiftUI
+
+struct Rahmattest1803: View {
+    var body: some View {
+        Text("inii updatean rahmat 1803")
+    }
+}
+
+struct Rahmattest1803_Previews: PreviewProvider {
+    static var previews: some View {
+        Rahmattest1803()
+    }
+}
