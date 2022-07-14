9dffbe5654eb710543bd68347327f5aa1d660a21
Nadia Ramadhani <103446529+twicetw@users.noreply.github.com>
2022-07-14 15:10:16 +0700

    test git

---
+++ NC3Facewash WatchKit Extension/Test git.swift
@@ -0,0 +1,20 @@
+//
+//  Test git.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Local Administrator on 14/07/22.
+//
+
+import SwiftUI
+
+struct Test_git: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test_git_Previews: PreviewProvider {
+    static var previews: some View {
+        Test_git()
+    }
+}
