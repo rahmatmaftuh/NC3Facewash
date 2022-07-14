20ef5889a1c155f4da6624196b97dc79bab75395
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-07-14 18:37:32 +0700

    NADIAJELEK1838

---
+++ NC3Facewash WatchKit Extension/Nadiajelek1838.swift
@@ -0,0 +1,20 @@
+//
+//  Nadiajelek1838.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Rahmat Maftuh Ihsan on 14/07/22.
+//
+
+import SwiftUI
+
+struct Nadiajelek1838: View {
+    var body: some View {
+        Text("NADIA JELEK 1838")
+    }
+}
+
+struct Nadiajelek1838_Previews: PreviewProvider {
+    static var previews: some View {
+        Nadiajelek1838()
+    }
+}
