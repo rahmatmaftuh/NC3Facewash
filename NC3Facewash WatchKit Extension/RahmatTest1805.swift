a938edf28cacf0d063f3cb51ed923b5ff0d6bd44
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-07-14 18:05:57 +0700

    rahmattestat1805WIB

---
+++ NC3Facewash WatchKit Extension/RahmatTest1805.swift
@@ -0,0 +1,20 @@
+//
+//  RahmatTest1805.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Rahmat Maftuh Ihsan on 14/07/22.
+//
+
+import SwiftUI
+
+struct RahmatTest1805: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RahmatTest1805_Previews: PreviewProvider {
+    static var previews: some View {
+        RahmatTest1805()
+    }
+}
