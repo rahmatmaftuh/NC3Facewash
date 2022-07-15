383028815a164db0b250a4c14c7c73b4d1c8d8ad
rahmatmaftuh <rahmatmaftuhi@gmail.com>
2022-07-15 10:14:39 +0700

    TimerFri15Jul1014

---
+++ NC3Facewash WatchKit Extension/Timer.swift
@@ -0,0 +1,20 @@
+//
+//  Timer.swift
+//  NC3Facewash WatchKit Extension
+//
+//  Created by Rahmat Maftuh Ihsan on 15/07/22.
+//
+
+import SwiftUI
+
+struct Timer: View {
+    var body: some View {
+        Text("Ini Timer")
+    }
+}
+
+struct Timer_Previews: PreviewProvider {
+    static var previews: some View {
+        Timer()
+    }
+}
